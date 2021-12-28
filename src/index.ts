a22e6025b75f43c97446170a47114036b615839b
Isichei Phelim <isicheiphelim@gmail.com>
2021-12-28 15:30:51 +0000

    setting jp my ts project

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log('Hello 2 from typescript');
+import 'dotenv';
+import app from './server';
+import { database } from './config/database';
+const PORT = 3000;
+
+app.listen(PORT, async () => {
+    await database()
+    console.log(`listening on: http://localhost:${PORT}`);
+})
