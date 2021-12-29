2621d2e71ea25cce1d395c0657b9f7024bf34fed
Isichei Phelim <isicheiphelim@gmail.com>
2021-12-29 09:55:57 +0000

    added the path

--- src/notes/notes.routes.ts
+++ src/notes/notes.routes.ts
@@ -1,11 +1,11 @@
 import express from 'express';
 import notesController from './notes.controller';
-import  Authentication  from 'src/middleware/users-auth.middleware';
-import  Authorization from 'src/middleware/authorize-middleware';
+import  Authentication  from '../middleware/users-auth.middleware';
+import  Authorization from '../middleware/authorize-middleware';
 
 const notesRouter = express.Router();
 
 
-notesRouter.route('/create').post(notesController.createNote);
+notesRouter.route('/create').post(Authentication.validateToken,Authorization.authorizeRoles('User'),notesController.createNote);
 
 export default notesRouter;
