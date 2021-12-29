0825b1e90cff8299a8307b09887249128f0c35e1
Isichei Phelim <isicheiphelim@gmail.com>
2021-12-29 09:28:26 +0000

    added the users-auth

---
+++ src/middleware/users-auth.middleware.ts
@@ -0,0 +1,38 @@
+import dotenv from "dotenv";
+dotenv.config();
+import jwt from "jsonwebtoken";
+import apiResponse from "../util/apiResponse";
+import {NextFunction, Request, Response,} from 'express';
+
+const validateToken = async (req:Request, res:Response, next: NextFunction) => {
+    const authorizationHeader = req.headers.authorization;
+    const authorizationType = req.headers.authtype;
+    let result:any;
+
+    if (authorizationHeader) {
+		const token = authorizationHeader.split(' ')[1]; // Bearer <token>
+		const options = {
+			expiresIn: process.env.JWT_EXPIRES_IN,
+			issuer: process.env.JWT_ISSUER,			
+		};
+        // console.log(token);
+        try {
+            // verify makes sure that the token hasn't expired and has been issued by us
+			result = jwt.verify(token, `${process.env.JWT_SECRET}`, options);
+
+			// Let's pass back the decoded token to the request object
+			req.decoded = result;
+
+			// We call next to pass execution to the subsequent middleware
+			return next();
+        } catch (error: any) {
+            console.log(error)
+            return apiResponse.errorResponse(res, error)
+             }
+        // return;
+    }
+    const message = "Authentication error; Token required.";
+    return apiResponse.errorResponse(res, message);
+};
+
+export { validateToken };
